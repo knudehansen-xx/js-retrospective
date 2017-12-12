d791803f46b951999026f73a451be1ffa1d63d42
knudehansen-xx <knudehansen@gmail.com>
2017-12-11 21:49:18 -0500

    Add files via upload

--- src/js/renderers/taskRenderer.js
+++ src/js/renderers/taskRenderer.js
@@ -1,6 +1,6 @@
-define(["jquery", ], function ($) {
+define(["jquery"], function ($) {
 
-    var taskTemplate = '<li class="task"><input class="complete" type="checkbox" /><input class="description" type="text" placeholder="Enter task description..." /><img src="css/delete-icon.png" alt="Delete Icon" style="width:28px;height:28px;"><button class="delete-button">Deleted</button></li>';
+    var taskTemplate = '<li class="task"><input class="complete" type="checkbox" /><input class="description" type="text" placeholder="Enter a good thing" /><img class="delete-button" src="css/delete-icon.png" alt="Delete Icon" style="width:28px;height:28px;"></li>';
 
     function renderTasks(tasks) {
         var elementArray = $.map(tasks, _renderTask);
